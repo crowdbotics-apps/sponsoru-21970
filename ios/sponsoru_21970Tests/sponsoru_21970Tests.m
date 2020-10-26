349a65106b56fa2bddf5de256675f3e2bbb978a1
Crowdbotics <team@crowdbotics.com>
2020-10-26 16:01:29 +0000

    Initial commit.

---
+++ ios/sponsoru_21970Tests/sponsoru_21970Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface sponsoru_21970 : XCTestCase
+
+@end
+
+@implementation sponsoru_21970
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
